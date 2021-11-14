504a45876fa68c5b9aa0b75c4276c960ffd4bca1
Yerong Li <yerong.li@outlook.com>
2021-11-13 21:19:00 -0600

    Reduce training batch in train_rams.sh

--- scripts/train_rams.sh
+++ scripts/train_rams.sh
@@ -7,8 +7,8 @@ CUDA_VISIBLE_DEVICES=1,2,3 PYTHONPATH=. python train.py --model=gen --ckpt_name=
     --train_file=data/RAMS_1.0/data/train.jsonlines \
     --val_file=data/RAMS_1.0/data/dev.jsonlines \
     --test_file=data/RAMS_1.0/data/test.jsonlines \
-    --train_batch_size=2 \
-    --eval_batch_size=4 \
+    --train_batch_size=1 \
+    --eval_batch_size=1 \
     --learning_rate=3e-5 \
     --accumulate_grad_batches=4 \
     --num_train_epochs=3 \
