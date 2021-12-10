a2f6cafe39a22c6334d76e4be72752ab9f3970cd
Yerong Li <yerong.li@outlook.com>
2021-12-10 13:03:17 -0600

    analysis

--- analysis.py
+++ analysis.py
@@ -1,9 +1,13 @@
-with open('/scratch/yerong/gen-arg/data/wikievents/test.jsonl', 'r') as json_file:
-    test_list = list(json_file)
+# with open('/scratch/yerong/gen-arg/data/wikievents/test.jsonl', 'r') as json_file:
+#     test_list = list(json_file)
 
-print(eval(test_list[0]).keys())
-print(eval(test_list[0])['doc_id'])
-print(eval(test_list[0])['entity_mentions'])
+# print(eval(test_list[0]).keys())
+# print(eval(test_list[0])['doc_id'])
+# print(eval(test_list[0])['entity_mentions'])
 
 with open('/scratch/yerong/gen-arg/checkpoints/gen-KAIROS-pred/predictions.jsonl', 'r') as json_file:
-    test_list = list(json_file)
+    predict_list = list(json_file)
+acc = 0.0
+for line in predict_list:
+    predicted = eval(line)['predicted']
+    print(predicted)
