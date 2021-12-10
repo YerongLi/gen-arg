71fa96ad49e8dfd589564e522ba066d0cc75c5c6
Yerong Li <yerong.li@outlook.com>
2021-12-10 12:59:37 -0600

    analysis

--- analysis.py
+++ analysis.py
@@ -3,5 +3,7 @@ with open('/scratch/yerong/gen-arg/data/wikievents/test.jsonl', 'r') as json_fil
 
 print(eval(test_list[0]).keys())
 print(eval(test_list[0])['doc_id'])
+print(eval(test_list[0])['entity_mentions'])
+
 with open('/scratch/yerong/gen-arg/checkpoints/gen-KAIROS-pred/predictions.jsonl', 'r') as json_file:
     test_list = list(json_file)
