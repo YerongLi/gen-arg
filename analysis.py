3ed953bbe16e474bff15ee661e1a70c312348550
Yerong Li <yerong.li@outlook.com>
2021-12-14 01:44:13 -0600

    Convert input tensor to list

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 with open('/scratch/yerong/gen-arg/data/wikievents/test.jsonl', 'r') as json_file:
     test_list = list(json_file)
-print(tokblue('test_list'))
+print('test_list')
 print(test_list[0])
 # print(eval(test_list[0]).keys())
 # print(eval(test_list[0])['doc_id'])
