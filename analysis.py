47e1ae957d534fce76d1a55526a894c2189981ed
Yerong Li <yerong.li@outlook.com>
2021-12-14 19:03:09 -0600

    keys of the test_list[0]

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 with open('/scratch/yerong/gen-arg/data/wikievents/test.jsonl', 'r') as json_file:
     test_list = list(json_file)
 print('test_list')
+print(test_list[0].keys())
 print(test_list[0])
 # print(eval(test_list[0]).keys())
 # print(eval(test_list[0])['doc_id'])
