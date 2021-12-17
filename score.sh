f9a9e5ac75e2b0e4d8431f88681d7b9dab844e16
Yerong Li <yerong.li@outlook.com>
2021-12-17 03:43:25 -0600

    Add scoring script

---
+++ score.sh
@@ -0,0 +1,1 @@
+git pull; /scratch/yerong/sha/bin/python src/genie/scorer.py --gen-file=checkpoints/gen-KAIROS-pred/predictions.jsonl --test-file=data/wikievents/test.jsonl --dataset=KAIROS --coref-file=data/wikievents/coref/test.jsonlines --head-only --coref
