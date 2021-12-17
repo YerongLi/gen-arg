9a68c06be019649cba9d90c9ec03855d7c4bcf42
Yerong Li <yerong.li@outlook.com>
2021-12-17 06:48:18 -0600

    debug: print indices

--- src/genie/scorer.py
+++ src/genie/scorer.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
 
     arg_idn_coref_num =0
     arg_class_coref_num =0
-    for ex in tqdm(list(examples.values())[:1]):
+    for ex in tqdm(list(examples.values())):
         context_words = ex['tokens']
         doc_id = ex['doc_id']
         doc = None 
